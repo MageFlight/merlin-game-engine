9464523e7efaa638977b76965b0fee350c308821
MageFlight <112671607+MageFlight@users.noreply.github.com>
2023-03-21 21:42:04 -0400

    Format webpack.config.js
    Format webpack.config.js using default Prettier VSCode settings so that
    it is easier to read and maintain. In the future, gts will probally be
    used as the formatter / linter.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/example/start.ts',
-    },
-    devtool: 'source-map', // Change this in production to 'source-map'
-    devServer: {
-        static: './dist',
-    },
-    module: {
-        rules: [
-          { 
-            test: /\.ts$/,
-            include: [path.resolve(__dirname, 'src')],
-            use: 'ts-loader',
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    }
-}
+  mode: "development",
+  entry: {
+    index: "./src/example/start.ts",
+  },
+  devtool: "source-map", // Change this in production to 'source-map'
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src")],
+        use: "ts-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
